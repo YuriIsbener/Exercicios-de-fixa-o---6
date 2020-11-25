3e8fc5b766921a6d7205c436698c2bb7bdd8655c
Yuri Isbener <yurisuhett@gmail.com>
2020-11-24 22:00:33 -0300

    trab feito

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicios_de_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write("Insira um nome: ");
+                nomes[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Que nome gostaria de consultar?");
+            string comparacao = Console.ReadLine().ToLower();
+            int contador = 0;
+
+            while (contador<10)
+            {
+                contador++;
+            if (nomes[contador] == comparacao)
+            {
+                Console.WriteLine("Achei");
+            }
+            else
+            {
+                Console.WriteLine("Não achei");
+            }
+            }
+        }
+    }
+}
